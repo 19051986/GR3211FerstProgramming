b9f3259983b0b974e566def768b65918f8a731d4
Alexey Zaitcev <alecs48.86@gmail.com>
2022-09-08 22:06:51 +0300

    Home

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿//==============================================================
+// #2 Напишите программу, которая на вход принимает два числа и
+// выдает, какое большее, а какое меньшее.
+//==============================================================
+
+string? inputLineN = Console.ReadLine()??"";
+string? inputLineM = Console.ReadLine()??"";
+
+int inputnumberN = int.Parse(inputLineN);
+int inputnumberM = int.Parse(inputLineM);
+
+if (inputnumberN > inputnumberM)
+{
+    Console.WriteLine("max N");
+}
+else
+{
+    Console.WriteLine("max M");
+}
+
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿//==============================================================
+// #4 Напишите программу, которая принимает на вход три числа и
+// выдает максимальное из этих чисел.
+//==============================================================
+
+string? inputLineA = Console.ReadLine() ?? "";
+string? inputLineB = Console.ReadLine() ?? "";
+string? inputLineC = Console.ReadLine() ?? "";
+
+int inputnumberA = int.Parse(inputLineA);
+int inputnumberB = int.Parse(inputLineB);
+int inputnumberC = int.Parse(inputLineC);
+int inputnumbermax = 0;
+
+inputnumbermax = inputnumberB > inputnumberA ? inputnumberB : inputnumberA;
+inputnumbermax = inputnumbermax > inputnumberC ? inputnumbermax : inputnumberC;
+
+Console.WriteLine(inputnumbermax);
+
