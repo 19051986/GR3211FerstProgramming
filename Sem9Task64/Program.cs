e99ae75fbae863db1dfe596dc3b0a13c65d71294
Alexey Zaitcev <alecs48.86@gmail.com>
2022-10-04 17:44:20 +0300

    Ninth seminar and homework

---
+++ Sem9Task64/Program.cs
@@ -0,0 +1,27 @@
+﻿// #64 Задайте значение N. Напишите программу, которая 
+// выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+// Печать строки.
+void PrintData(string data, string pref = "", string post = "", string end = "\n")
+{
+    Console.Write($"{pref}{data}{post}{end}");
+}
+
+// Метод считывания данных пользователя
+uint ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Возвращаем значение
+    return uint.Parse(Console.ReadLine() ?? "");
+}
+
+// Реверсивная последовательность чисел.
+string SequenceRevers(uint num)
+{
+    if (num == 0) return string.Empty;
+    return $"{num} {SequenceRevers(num - 1)}";
+}
+
+PrintData(SequenceRevers(ReadData("Введите число: ")));
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,29 @@
+﻿// #68 Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+
+// Печать строки.
+void PrintData(string data, string pref = "", string post = "", string end = "\n")
+{
+    Console.Write($"{pref}{data}{post}{end}");
+}
+
+// Метод считывания данных пользователя
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Возвращаем значение
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+// Функция Аккермана.
+int AckermanRec(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return AckermanRec(m - 1, 1);
+    return AckermanRec(m - 1, (int)AckermanRec(m, n - 1));
+}
+
+int m = ReadData("Введите число m: ");
+int n = ReadData("Введите число n: ");
+PrintData(AckermanRec(m,n).ToString());
